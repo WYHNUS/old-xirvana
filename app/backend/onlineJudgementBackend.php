5645b6e29616921af4044eabb0d2dd2d1f014b24
WYHNUS <wangyanhao1993@163.com>
2016-01-18 00:15:09 +0800

    add comparision btw user's output and correct output stub

--- app/backend/onlineJudgementBackend.php
+++ app/backend/onlineJudgementBackend.php
@@ -23,29 +23,51 @@ if ($_FILES["file"]) {
         passthru("javac ".$program_full_path, $return_value);
         
         if ($return_value === 0) {
+            // get number of test cases
+            $change_to_input_dir = "cd ".$practice_dir."/testdata/input; ";
+            $get_num_cmd = "numfiles=(*); numfiles=\${#numfiles[@]}; echo \$numfiles; ";
+            $num_of_tests = exec($change_to_input_dir.$get_num_cmd);
+                
             // check if submitted java file passes all the test cases
-            $change_dir_cmd = "cd ".$save_to_dir;
+            $change_to_tmp_dir = "cd ".$save_to_dir."; ";
+            $input_dir = "../testdata/input/";
+            $out_dir = "../testdata/output/";
             $set_limit_cmd = "ulimit -v 4000; ulimit -t 2; ";
             
-            $input_dir = "../testdata/input/";
-            $sample_out_dir = "../testdata/output/";
-            $exec_cmd = "java ".$program_name." < ".$input_dir."sum_integers1.in"." > "."tmp.out";
-            passthru($change_dir_cmd."; ".$set_limit_cmd.$exec_cmd, $return_value);
-            
-            if ($return_value === 0) {
-                print_jsonp_callback("{\"status\":\"ok\"}");
-            } else {
-                print_jsonp_callback("{\"status\":\"fail\",\"message\":\"run time error\"}");
+            for ($test_index=1; $test_index<=$num_of_tests; $test_index++) {
+                $input_file = "in".$test_index.".in";
+                $output_file = "out".$test_index.".out";
+                $tmp_out_file = "tmpout".$test_index.".out";
+                $exec_cmd = "java ".$program_name." < ".$input_dir.$input_file." > ".$tmp_out_file."; ";
+                passthru($change_to_tmp_dir.$set_limit_cmd.$exec_cmd, $return_value);
+
+                if ($return_value === 0) {
+                    $compare_cmd = "diff -b ".$tmp_out_file." ".$out_dir.$output_file;
+                    $result = exec($compare_cmd, $outcome);
+                    error_log($compare_cmd);
+                    error_log($result);
+                    error_log(count($outcome));
+//                    if () {
+//                        print_jsonp_callback("{\"status\":\"fail\",\"message\":\"incorrect result\"}");
+//                        exit();
+//                    }
+                } else {
+                    print_jsonp_callback("{\"status\":\"fail\",\"message\":\"run time error\"}");
+                    exit();
+                }
             }
+            
+            // pass all the test cases
+            print_jsonp_callback("{\"status\":\"ok\"}");
         } else {
             print_jsonp_callback("{\"status\":\"fail\",\"message\":\"compilation error\"}");
         }
         
         // clean up
-        passthru($change_dir_cmd."; "."rm *.*", $return_value);
-        if ($return_value != 0) {
-            error_log("clean up failed");
-        }
+//        passthru($change_to_tmp_dir."rm *.*", $return_value);
+//        if ($return_value != 0) {
+//            error_log("clean up failed");
+//        }
     } else {
         print_jsonp_callback("{\"status\":\"fail\",\"message\":\"fail to save input file\"}");
     }
--- assets/Practice/test.sh
+++ assets/Practice/test.sh
@@ -14,17 +14,34 @@
 #	done
 
 for i in {1..43}
-    do
-        if (( $i <= 9 ))
-        then 
-            index="0$i"
-        else
-            index="$i"
-        fi
+   do
+       if (( $i <= 9 ))
+       then 
+           index="0$i"
+       else
+           index="$i"
+       fi
         dir_name="practice${index}"
-        new_dir_name="tmp"
-        cd $dir_name
-        mkdir $new_dir_name
-        chmod 777 tmp
-        cd ..
+        # new_dir_name="tmp"
+        # cd $dir_name
+        # mkdir $new_dir_name
+        # chmod 777 tmp
+        # cd ..
+
+        cd "${dir_name}/testdata/output"
+        j=1
+        for file in *.out
+        do
+        	name=${file%%[.]*}
+        	if (($j == 1))
+        		then
+		        	name_length=${#name}
+		        	common_name=${name:0:${name_length}-1}
+        	fi
+        	# get index of test file
+        	output_index=${name##$common_name}
+        	mv "${common_name}${output_index}.out" "out${output_index}.out"
+        	j=$(($j+1))
+        done
+        cd ../../..
     done
