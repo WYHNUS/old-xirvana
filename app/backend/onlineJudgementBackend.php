f0aa5d116ef578c88cb15d79e977f0954e40825d
WYHNUS <wangyanhao1993@163.com>
2016-01-17 13:44:07 +0800

    enable execution of compiled java file; implement cleanup of tmp folder

--- app/backend/onlineJudgementBackend.php
+++ app/backend/onlineJudgementBackend.php
@@ -24,22 +24,28 @@ if ($_FILES["file"]) {
         
         if ($return_value === 0) {
             // check if submitted java file passes all the test cases
-            $change_dir_cmd = "cd /".$save_to_dir;
+            $change_dir_cmd = "cd ".$save_to_dir;
+            $set_limit_cmd = "ulimit -v 4000; ulimit -t 2; ";
             
             $input_dir = "../testdata/input/";
             $sample_out_dir = "../testdata/output/";
             $exec_cmd = "java ".$program_name." < ".$input_dir."sum_integers1.in"." > "."tmp.out";
-            $process = new ShellProcess($change_dir_cmd."; ".$exec_cmd);
-            error_log($process->getPid());
+            passthru($change_dir_cmd."; ".$set_limit_cmd.$exec_cmd, $return_value);
             
-            print_jsonp_callback("{\"status\":\"ok\"}");
+            if ($return_value === 0) {
+                print_jsonp_callback("{\"status\":\"ok\"}");
+            } else {
+                print_jsonp_callback("{\"status\":\"fail\",\"message\":\"run time error\"}");
+            }
         } else {
             print_jsonp_callback("{\"status\":\"fail\",\"message\":\"compilation error\"}");
         }
         
         // clean up
-        //$process = new ShellProcess("rm -rf -- ".$save_to_dir);
-        //$process = new ShellProcess("mkdir ".$save_to_dir);
+        passthru($change_dir_cmd."; "."rm *.*", $return_value);
+        if ($return_value != 0) {
+            error_log("clean up failed");
+        }
     } else {
         print_jsonp_callback("{\"status\":\"fail\",\"message\":\"fail to save input file\"}");
     }
