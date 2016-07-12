57be01230b4082e737d00ea46b98e52542424487
WYHNUS <wangyanhao1993@163.com>
2016-07-12 19:30:36 +0800

    Disable multiple queries before returning results for retrieving modules taken page

--- app/components/modulesTaken/modulesTakenController.js
+++ app/components/modulesTaken/modulesTakenController.js
@@ -3,17 +3,25 @@
 angular.module("xirvanaApp")
     .controller("modulesTakenController", ["$scope", "ConnectDBService",
     function($scope, ConnectDBService) {
+        $scope.isRetrieving = false;
+        
         $scope.retrieve = function() {
             console.log("start crawling...");
             $scope.errorMsg = "";
             
-            ConnectDBService.crawlNUSTakenModule($scope.user).then(function(response) {
-                console.log(response.msg);
-                $scope.takenModules = JSON.parse(response.msg);
-                console.log($scope.takenModules);
-            }, function(err) {
-                $scope.errorMsg = err;
-                console.log($scope.errorMsg);
-            })
+            if (!$scope.isRetrieving) {
+                $scope.isRetrieving = true;
+                ConnectDBService.crawlNUSTakenModule($scope.user).then(function(response) {
+                    $scope.takenModules = JSON.parse(response.msg);
+                    console.log($scope.takenModules);
+                    $scope.isRetrieving = false;
+                }, function(err) {
+                    $scope.errorMsg = err;
+                    console.log($scope.errorMsg);
+                    $scope.isRetrieving = false;
+                })
+            } else {
+                $scope.errorMsg = "We are trying our best to retrive the data, please wait a moment. :)";
+            }
         } 
     }]);
